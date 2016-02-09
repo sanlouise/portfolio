eddc6edb286c7c8a286ac6db402d09747d11bb1e
sandralouise <sandra.adamshallie@gmail.com>
2016-02-09 11:20:00 +0000

    Add pages and styling in views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,16 @@ class PagesController < ApplicationController
   def home
   end
   
+  def about
+  end
+  
+  def portfolio 
+  end 
+  
+  def blog 
+  end
+  
+  def contact
+  end 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   
+  get '/portfolio', to: 'pages#portfolio'
+  get '/about', to: 'pages#about' 
+  get '/blog', to: 'pages#blog' 
+  get '/contact', to: 'pages#contact' 
+  
 end
