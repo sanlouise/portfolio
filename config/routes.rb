195169e99f73b9ba2274c0207f523ab4512c1e4d
sandralouise <sandra.adamshallie@gmail.com>
2016-02-10 16:26:05 +0000

    Update homepage view

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about' 
   get '/blog', to: 'pages#blog' 
   get '/contact', to: 'pages#contact' 
-  
-  resources :contacts
+
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210141044) do
-
-  create_table "contacts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.text     "comments"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
