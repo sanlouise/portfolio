429c18393dd1c3eaa2d4b2e7fb03aa0360af9ed1
sandralouise <sandra.adamshallie@gmail.com>
2016-04-08 19:09:40 +0000

    Update validations for blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,6 @@
 class Blog < ActiveRecord::Base
 
-  validates :title, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :title, presence: true, length: { minimum: 2}
   validates :body, presence: true, length: { minimum: 2 }
   
 end
