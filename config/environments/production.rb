a24fd9bdbee2e6a4faa0a67cb9c02465149a1f6b
sandralouise <sandra.adamshallie@gmail.com>
2016-06-01 06:47:04 +0000

    Update production.rb for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
+  
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.precompile += ['static_pages.css']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
