d06463a0c01fe238ad2b40716e46fba4860c1cab
sandralouise <sandra.adamshallie@gmail.com>
2016-06-10 17:57:51 +0000

    Add pagination for blog postings

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,7 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all
-    @blogs = Blog.paginate(page: params[:page], per_page: 20)
+    @blogs = Blog.paginate(page: params[:page], per_page: 8)
 
   end
   
