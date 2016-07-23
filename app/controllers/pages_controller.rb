07a50fbca995b46667d5caacb117328a302e0e41
sanlouise <sandra.adamshallie@gmail.com>
2016-07-23 14:33:45 +0200

    Remove unused actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,28 +6,8 @@ class PagesController < ApplicationController
   def about
   end
   
-  def portfolio 
-  end 
-  
-  def portfolio2
-  end
-  
-  def blog 
-  end
-  
-  def contact
-  end 
-  
   def search
     @blogs = Blog.search(params[:search_blogs]).order("created_at DESC")
-  end
-  
-  def ios_projects
-    @ios_projects = IosProject.all 
-  end
-  
-  def rails_projects
-    @rails_projects = RailsProject.all
   end
   
 end
