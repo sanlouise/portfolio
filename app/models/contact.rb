e01e3ed48bc5abd5477b2eaa86ab2f6546e9fd70
sanlouise <sandra.adamshallie@gmail.com>
2016-07-23 14:34:54 +0200

    Validate email with regex and add length validations for name and comments

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,12 @@
 class Contact < ActiveRecord::Base
   
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :comments, presence: true
+  validates :name, presence: true, length: {maximum: 150}
+  validates :comments, presence: true, length: {maximum: 5000}
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 },
+                      uniqueness: { case_sensitive: false },
+                      format: { with: VALID_EMAIL_REGEX }
 
 end
 
