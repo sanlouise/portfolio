6526d3ca94c4f675532017e57751f46afee7ea1d
sanlouise <sandra.adamshallie@gmail.com>
2016-07-23 14:34:07 +0200

    Add javascript, ios, and rails routes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,5 +3,17 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.order('title ASC').paginate(page: params[:page], per_page: 10)
   end
+
+  def javascript
+  	@js_projects = Project.where('project_type = "JavaScript"').paginate(page: params[:page], per_page: 10)
+  end
+
+  def ios
+  	@ios_projects = Project.where('project_type = "iOS"').paginate(page: params[:page], per_page: 10)
+  end
+
+  def rails
+  	@rails_projects = Project.where('project_type = "Ruby on Rails"').paginate(page: params[:page], per_page: 10)
+  end
   
 end
