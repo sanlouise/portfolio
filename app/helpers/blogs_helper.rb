2510279df3e9d32af5b7f86561d445753f0ec876
sanlouise <sandra.adamshallie@gmail.com>
2016-07-24 21:58:03 +0200

    Add create_blog_file blogs helper method

---
+++ app/helpers/blogs_helper.rb
@@ -0,0 +1,14 @@
+module BlogsHelper
+
+	def create_blog_file(blog)
+
+		title = blog.title.downcase.gsub(/[ ]/, '_')
+
+		blog_file = File.new("db/blog_posts/#{title}.rb", 'w+')
+
+		blog_file.puts "Blog.create!(\n\ttitle: '#{blog.title}',\n\tbody: '#{blog.body}',\n\ttopic: '#{blog.topic}',\n\tcreated_at: '#{blog.created_at}'\n)"
+
+		blog_file.close
+	end
+
+end
