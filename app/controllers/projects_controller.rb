cfc0964ec28160837cf2f9cbc804e1cb8ae3c2bf
sanlouise <sandra.adamshallie@gmail.com>
2016-07-25 12:45:16 +0200

    Fix bug

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,15 +5,15 @@ class ProjectsController < ApplicationController
   end
 
   def javascript
-  	@js_projects = Project.where('project_type = "JavaScript"').paginate(page: params[:page], per_page: 10)
+  	@js_projects = Project.where(project_type: "JavaScript").paginate(page: params[:page], per_page: 10)
   end
 
   def ios
-  	@ios_projects = Project.where('project_type = "iOS"').paginate(page: params[:page], per_page: 10)
+  	@ios_projects = Project.where(project_type: "iOS").paginate(page: params[:page], per_page: 10)
   end
 
   def rails
-  	@rails_projects = Project.where('project_type = "Ruby on Rails"').paginate(page: params[:page], per_page: 10)
+  	@rails_projects = Project.where(project_type: "Ruby on Rails").paginate(page: params[:page], per_page: 10)
   end
   
 end
