1ed108dab17c6fa38a050a2ea3f3c3d489e28385
sanlouise <sandra.adamshallie@gmail.com>
2016-07-26 20:08:40 +0200

    Install shoulda and Rspec gems, create rails_helper and spec_helper, add specs for projects_controller

--- Gemfile
+++ Gemfile
@@ -27,15 +27,20 @@ gem 'will_paginate-materialize'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
-  # Use sqlite3 as the database for Active Record
-  gem 'sqlite3'
-  # Access an IRB console on exception pages or by using <%= console %> in views
+group :test do
+  gem 'shoulda'
+end
+
+group :development do
   gem 'web-console', '~> 2.0'
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+end
+
+group :development, :test do
+  gem 'byebug'
+  gem 'sqlite3'
   gem 'spring'
+  gem 'rspec-rails'
+  gem 'shoulda-matchers'
 end
 
 group :production do
---
+++ spec/project_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  
+
+  describe 'its title' do
+    it do
+      should validate_presence_of(:title)
+      should validate_length_of(:title).is_at_least(2)
+      should validate_length_of(:title).is_at_most(22)
+    end
+  end
+
+  describe 'its points' do
+    it do
+      should validate_presence_of(:points)
+    end
+  end
+
+  describe 'its description' do
+    it do
+      should validate_presence_of(:description)
+      should validate_length_of(:description).is_at_least(2)
+      should validate_length_of(:description).is_at_most(120)
+    end
+  end
+
+  describe 'its git_link' do
+    it do
+      should validate_length_of(:github).is_at_least(5)
+      should validate_length_of(:github).is_at_most(100)
+    end
+  end
+
+  describe 'its category' do
+    it do
+      should validate_presence_of(:project_type)
+      should validate_length_of(:project_type).is_at_least(2)
+      should validate_length_of(:project_type).is_at_most(60)
+    end
+  end
+
+end
