8415af693a1ffafc87313bf412a187e5c4e0ebf4
sanlouise <sandra.adamshallie@gmail.com>
2016-07-28 12:00:27 +0200

    Fix contacts model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ActiveRecord::Base
   
   validates :name, presence: true, length: {maximum: 150}
-  validates :comments, presence: true, length: {maximum: 1000}, exclusion: 
+  validates :comments, presence: true, length: {maximum: 1000}
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 150 },
